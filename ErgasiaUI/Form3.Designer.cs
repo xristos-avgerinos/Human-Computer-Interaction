50e6855769a3c4e682f282460771d48909a904fe
Vit <nassos1944@hotmail.com>
2022-01-20 10:39:24 +0200

    debug added

--- ErgasiaUI/Form3.Designer.cs
+++ ErgasiaUI/Form3.Designer.cs
@@ -130,7 +130,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 474);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
